d7ac20a1efcec82ad5075e973a53c87ca72c257c
Siegel Rebeka <rebeka.siegel@stud.ubbcluj.ro>
2025-03-12 22:25:08 +0200

    initial next.js setup

--- README.md
+++
@@ -1,1 +0,0 @@
-# Allamvizsga - cukraszda weboldal
---
+++ gertis-dessert-island/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Gerti's Dessert Island",
+  description: "A website about Gerti's dessert island",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
